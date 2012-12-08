1b336353a88d03d3f1c5d457447c20b4c2297032
alex <alex@localhost.localdomain>
2012-12-08 04:04:06 +0400

    merging with rosa2012.1 of project apache-mod_auth_remote

--- apache-mod_auth_remote.spec
+++ apache-mod_auth_remote.spec
@@ -1,21 +1,27 @@
 #Module-Specific definitions
-%define apache_version 2.4.0
+%define apache_version 2.2.4
 %define mod_name mod_auth_remote
-%define load_order 182
+%define mod_conf 82_%{mod_name}.conf
+%define mod_so %{mod_name}.so
 
 Summary:	DSO module for the apache Web server
 Name:		apache-%{mod_name}
 Version:	0.2
-Release:	2
+Release:	%mkrel 0.9
 Group:		System/Servers
 License:	Apache License
 URL:		http://saju.pillai.googlepages.com/mod_auth_remote
-Source0:	https://raw.github.com/saju/mod_auth_remote/master/src/mod_auth_remote.c
+Source0:	http://github.com/saju/mod_auth_remote/raw/dd48860bdca8366df1d93cd5df66a128278b6104/src/mod_auth_remote.c
+Source1:	%{mod_conf}
 Requires(pre): rpm-helper
 Requires(postun): rpm-helper
+Requires(pre):  apache-conf >= %{apache_version}
+Requires(pre):  apache >= %{apache_version}
+Requires:	apache-conf >= %{apache_version}
 Requires:	apache >= %{apache_version}
 BuildRequires:  apache-devel >= %{apache_version}
 Epoch:		1
+BuildRoot:	%{_tmppath}/%{name}-%{version}-%{release}-buildroot
 
 %description
 This module is a very simple, lightweight method of setting up a single signon
@@ -36,29 +42,195 @@ responses the client is not validated.
 %setup -q -T -c -n %{mod_name}-%{version}
 
 cp %{SOURCE0} %{mod_name}.c
+cp %{SOURCE1} %{mod_conf}
 
 %build
-apxs -c %{mod_name}.c
+%{_bindir}/apxs -c %{mod_name}.c
 
 %install
+[ "%{buildroot}" != "/" ] && rm -rf %{buildroot}
 
-install -d %{buildroot}%{_libdir}/apache
+install -d %{buildroot}%{_libdir}/apache-extramodules
 install -d %{buildroot}%{_sysconfdir}/httpd/modules.d
 
-install -m0755 .libs/*.so %{buildroot}%{_libdir}/apache/
-
-cat > %{buildroot}%{_sysconfdir}/httpd/modules.d/%{load_order}_%{mod_name}.conf << EOF
-LoadModule auth_remote_module %{_libdir}/apache/%{mod_name}.so
-EOF
+install -m0755 .libs/*.so %{buildroot}%{_libdir}/apache-extramodules/
+install -m0644 %{mod_conf} %{buildroot}%{_sysconfdir}/httpd/modules.d/%{mod_conf}
 
 %post
-/bin/systemctl daemon-reload >/dev/null 2>&1 || :
+if [ -f %{_var}/lock/subsys/httpd ]; then
+    %{_initrddir}/httpd restart 1>&2;
+fi
 
 %postun
 if [ "$1" = "0" ]; then
-    /bin/systemctl daemon-reload >/dev/null 2>&1 || :
+    if [ -f %{_var}/lock/subsys/httpd ]; then
+        %{_initrddir}/httpd restart 1>&2
+    fi
 fi
 
+%clean
+[ "%{buildroot}" != "/" ] && rm -rf %{buildroot}
+
 %files
-%attr(0644,root,root) %config(noreplace) %{_sysconfdir}/httpd/modules.d/*.conf
-%attr(0755,root,root) %{_libdir}/apache/*.so
+%defattr(-,root,root)
+%attr(0644,root,root) %config(noreplace) %{_sysconfdir}/httpd/modules.d/%{mod_conf}
+%attr(0755,root,root) %{_libdir}/apache-extramodules/%{mod_so}
+
+
+%changelog
+* Sat May 14 2011 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.7mdv2011.0
++ Revision: 674423
+- rebuild
+
+* Mon May 02 2011 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.6
++ Revision: 662771
+- mass rebuild
+
+* Sun Oct 24 2010 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.5mdv2011.0
++ Revision: 588277
+- rebuild
+
+* Mon Mar 08 2010 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.4mdv2010.1
++ Revision: 515832
+- rebuilt for apache-2.2.15
+
+* Wed Sep 30 2009 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.3mdv2010.0
++ Revision: 451696
+- rebuild
+
+* Fri Jul 31 2009 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.2mdv2010.0
++ Revision: 405127
+- rebuild
+
+* Sun Mar 22 2009 Oden Eriksson <oeriksson@mandriva.com> 1:0.2-0.1mdv2009.1
++ Revision: 360422
+- 0.2 (beta)
+
+* Wed Jan 07 2009 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-18mdv2009.1
++ Revision: 326480
+- rebuild
+
+* Mon Jul 14 2008 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-17mdv2009.0
++ Revision: 235637
+- rebuild
+
+* Thu Jun 05 2008 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-16mdv2009.0
++ Revision: 215287
+- rebuild
+
+* Fri Mar 07 2008 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-15mdv2008.1
++ Revision: 181436
+- rebuild
+
+* Mon Feb 18 2008 Thierry Vignaud <tv@mandriva.org> 1:0.1-14mdv2008.1
++ Revision: 170709
+- rebuild
+- fix "foobar is blabla" summary (=> "blabla") so that it looks nice in rpmdrake
+
+* Fri Jan 11 2008 Thierry Vignaud <tv@mandriva.org> 1:0.1-13mdv2008.1
++ Revision: 148460
+- rebuild
+- kill re-definition of %%buildroot on Pixel's request
+
+  + Olivier Blin <oblin@mandriva.com>
+    - restore BuildRoot
+
+* Sat Sep 08 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-12mdv2008.0
++ Revision: 82357
+- rebuild
+
+* Thu Aug 16 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-11mdv2008.0
++ Revision: 64317
+- use the new %%serverbuild macro
+
+* Wed Jun 13 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-10mdv2008.0
++ Revision: 38409
+- rebuild
+
+
+* Sat Mar 10 2007 Oden Eriksson <oeriksson@mandriva.com> 0.1-9mdv2007.1
++ Revision: 140579
+- rebuild
+
+* Tue Feb 27 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-8mdv2007.1
++ Revision: 126607
+- general cleanups
+
+* Sat Feb 10 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-7mdv2007.1
++ Revision: 118723
+- bump release
+- do mdvsys sync...
+
+* Sat Feb 10 2007 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-6mdv2007.1
++ Revision: 118714
+- fix url
+
+* Thu Nov 09 2006 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-5mdv2007.1
++ Revision: 79234
+- Import apache-mod_auth_remote
+
+* Sun Jul 30 2006 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-5mdv2007.0
+- rebuild
+
+* Thu Dec 15 2005 Oden Eriksson <oeriksson@mandriva.com> 0.1-4mdk
+- rebuilt against apache-2.2.0
+- added P1 to fix warnings (pterjan)
+- added P2 to conform with apr1
+
+* Sun Oct 30 2005 Oden Eriksson <oeriksson@mandriva.com> 0.1-3mdk
+- rebuilt to provide a -debug package too
+
+* Mon Oct 17 2005 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-2mdk
+- rebuilt against correct apr-0.9.7
+
+* Sat Oct 15 2005 Oden Eriksson <oeriksson@mandriva.com> 1:0.1-1mdk
+- rebuilt for apache-2.0.55
+
+* Sat Jul 30 2005 Oden Eriksson <oeriksson@mandriva.com> 2.0.54_0.1-3mdk
+- added another work around for a rpm bug
+
+* Sat Jul 30 2005 Oden Eriksson <oeriksson@mandriva.com> 2.0.54_0.1-2mdk
+- added a work around for a rpm bug, "Requires(foo,bar)" don't work
+
+* Fri May 27 2005 Oden Eriksson <oeriksson@mandriva.com> 2.0.54_0.1-1mdk
+- rename the package
+- use the %%mkrel macro
+- the conf.d directory is renamed to modules.d
+- use new rpm-4.4.x pre,post magic
+
+* Sun Feb 27 2005 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.53_0.1-5mdk
+- fix %%post and %%postun to prevent double restarts
+
+* Wed Feb 16 2005 Stefan van der Eijk <stefan@eijk.nu> 2.0.53_0.1-4mdk
+- fix bug #6574
+
+* Wed Feb 16 2005 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.53_0.1-3mdk
+- fix deps
+
+* Tue Feb 15 2005 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.53_0.1-2mdk
+- spec file cleanups, remove the ADVX-build stuff
+
+* Tue Feb 08 2005 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.53_0.1-1mdk
+- rebuilt for apache 2.0.53
+
+* Wed Sep 29 2004 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.52_0.1-1mdk
+- built for apache 2.0.52
+
+* Fri Sep 17 2004 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.51_0.1-1mdk
+- built for apache 2.0.51
+
+* Wed Aug 11 2004 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.50_0.1-3mdk
+- rebuilt
+
+* Tue Jul 13 2004 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.50_0.1-2mdk
+- remove redundant provides
+
+* Thu Jul 01 2004 Oden Eriksson <oeriksson@mandrakesoft.com> 2.0.50_0.1-1mdk
+- built for apache 2.0.50
+
+* Sat Jun 12 2004 Oden Eriksson <oden.eriksson@kvikkjokk.net> 2.0.49_0.1-1mdk
+- built for apache 2.0.49
+- used a sligthly newer source
+- v1.0 was wrong, should have been v0.1, fixed now
+- fixed P0
+
---
+++ mod_auth_remote.c
@@ -0,0 +1,360 @@
+/*
+ * mod_auth_remote - Remote authentication module for apache httpd 2.2
+ *
+ * saju.pillai@gmail.com
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+#include <apr_strings.h>
+#include <apr_uri.h>
+#include <apr_base64.h>
+#include <apr_md5.h>
+#include <apr_time.h>
+#include <apr_lib.h>
+#include <httpd.h>
+#include <http_config.h>
+#include <http_log.h>
+#include <ap_provider.h>
+#include <mod_auth.h>
+
+#define NOT_CONFIGURED  -42
+#define TWENTY_MINS     1200
+#define DEFAULT_TIMEOUT 5000000
+#define AUTH_REMOTE_COOKIE "auth_remote_cookie"
+
+unsigned char auth_remote_salt[8];
+short want_salt = 0;
+
+typedef struct {
+  int remote_port;                 /* the remote port of the authenticating server */
+  int cookie_life;                 /* the duration for which the cookie should live */
+  const char *remote_server;       /* hostname/ip for the remote server */
+  const char *remote_path;         /* the protected resource on the remote server */
+  const char *cookie_name;         /* the name of the cookie */
+  const char *cookie_path;         /* the cookie path */
+} auth_remote_config_rec;
+
+
+module AP_MODULE_DECLARE_DATA auth_remote_module;
+
+static void *create_auth_remote_dir_config(apr_pool_t *p, char *d) 
+{
+  auth_remote_config_rec *conf = apr_palloc(p, sizeof(*conf));
+  conf->remote_port = NOT_CONFIGURED;
+  conf->cookie_life = NOT_CONFIGURED;
+  conf->remote_server = NULL;
+  conf->remote_path = NULL;
+  conf->cookie_name = NULL;
+  conf->cookie_path = NULL;
+  
+  want_salt = 1;
+
+  return conf;
+}
+
+static const char *auth_remote_parse_loc(cmd_parms *cmd, void *config, const char *arg)
+{
+  apr_uri_t uri;
+  auth_remote_config_rec *conf = config;
+  apr_status_t rv = apr_uri_parse(cmd->pool, arg, &uri);
+  if (rv != APR_SUCCESS )
+    return "AuthRemoteURL should an URL or path to the authenticating server";
+  
+  if (!uri.scheme) {
+    conf->remote_path = arg;
+  } 
+  else {    
+    if (strncmp(uri.scheme , "http", 4))
+      return "AuthRemoteURL must be a http uri";
+    
+    conf->remote_server = uri.hostname;
+    conf->remote_port = uri.port ? uri.port : 80;
+    if (!uri.path)
+      conf->remote_path = "/";
+    else {
+      conf->remote_path = uri.path;
+      if (uri.query) 
+        conf->remote_path = apr_pstrcat(cmd->pool, conf->remote_path, "?", uri.query, NULL);
+      if (uri.fragment)
+        conf->remote_path = apr_pstrcat(cmd->pool, conf->remote_path, "#", uri.fragment, NULL);
+    }
+  }
+  return NULL;
+}
+
+static const char *auth_remote_config_cookie(cmd_parms *cmd, void *config, const char *arg1, 
+                                             const char *arg2, const char *arg3)
+{
+#ifdef AUTH_REMOTE_NO_SALT
+  return "AuthRemoteCookie can only be used if mod_auth_remote is compiled with random() support";
+#else  
+  auth_remote_config_rec *conf = config;
+  conf->cookie_name = arg1;
+  if (arg2)
+    conf->cookie_path = arg2;
+  else 
+    conf->cookie_path = "/";
+  if (arg3) 
+    conf->cookie_life = atoi(arg3);
+  else
+    conf->cookie_life = TWENTY_MINS;
+  
+  return NULL;
+#endif
+}
+
+static const command_rec auth_remote_cmds[] = 
+  {
+    /* for backward compatibility */
+    AP_INIT_TAKE1("AuthRemotePort", ap_set_int_slot, 
+                  (void *)APR_OFFSETOF(auth_remote_config_rec, remote_port),
+                  OR_AUTHCFG, "remote port to authenticate against"),
+    /* for backward compatibility */
+    AP_INIT_TAKE1("AuthRemoteServer", ap_set_string_slot, 
+                  (void *)APR_OFFSETOF(auth_remote_config_rec, remote_server),
+                  OR_AUTHCFG, "remote server to authenticate against"),
+    /* accepts a full url, superceedes AuthRemotePort and AuthRemoteServer */
+    AP_INIT_TAKE1("AuthRemoteURL", auth_remote_parse_loc, NULL, OR_AUTHCFG,
+                  "remote server path or full url to authenticate against"),
+    AP_INIT_TAKE123("AuthRemoteCookie", auth_remote_config_cookie, NULL, OR_AUTHCFG,
+                    "name of the cookie, the cookie path and the duration it is valid for"),
+    {NULL}
+  };
+
+
+static char  *auth_remote_signature(apr_pool_t *p, const char *user, apr_int64_t curr, 
+                                    unsigned char *salt)
+{
+  int blen = apr_base64_encode_len(APR_MD5_DIGESTSIZE);
+  unsigned char md5[APR_MD5_DIGESTSIZE];
+  char *md5_b64 = apr_palloc(p, blen);
+  char *s = apr_psprintf(p, "%s:%lld:%s", user, curr, salt);
+  
+  apr_md5(md5, s, strlen(s));
+  apr_base64_encode_binary(md5_b64, md5, APR_MD5_DIGESTSIZE);
+  return md5_b64;
+}
+
+static short auth_remote_validate_cookie(request_rec *r, const char *exp_user, char *cookie, 
+                                         auth_remote_config_rec *conf)
+{
+  /*
+    our cookie looks like this ...
+    NAME=USER^TSTAMP^MD5(USER:TSTAMP:salt)
+  */
+  apr_time_t new_time = apr_time_sec(apr_time_now());
+  char *payload = apr_pstrdup(r->pool, cookie + strlen(conf->cookie_name) + 1);
+  char *last, *user, *tstamp, *sig, *nsig;
+  apr_int64_t old_time;
+
+  user = apr_strtok(payload, "^", &last);
+  if (!user) {
+    ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, 
+                  "%s - tampering(?). \"user\" missing from cookie", exp_user);
+    return 0;
+  } else if (strncmp(exp_user, user, strlen(exp_user))) {
+    ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, 
+                  "%s - tampering(?). \"user\" (%s) mismatch in cookie", exp_user, user);
+    return 0;
+  }
+  
+  tstamp = apr_strtok(NULL, "^", &last);
+  if (!tstamp) {
+    ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, 
+                  "%s - tampering(?). \"tstamp\" missing from cookie", exp_user);
+    return 0;
+  }
+  old_time = apr_atoi64(tstamp);
+  if ((new_time - old_time) > conf->cookie_life) {
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r, "cookie expired for %s", exp_user);
+    return 0;
+  }
+  
+  sig = apr_strtok(NULL, "^", &last);
+  if (!sig) {
+    ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, 
+                  "%s - tampering(?). \"signature\" missing from cookie", exp_user);
+    return 0;
+  }
+  nsig = auth_remote_signature(r->pool, user, old_time, auth_remote_salt);
+  if (strncmp(sig, nsig, strlen(nsig))) {
+    ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, 
+                  "%s - tampering(?). \"signature\" mismatch in cookie", exp_user);
+    return 0;
+  }
+  return 1;
+}
+
+static void auth_remote_set_cookie(request_rec *r, const char *user, auth_remote_config_rec *conf)
+{
+  apr_time_t now = apr_time_sec(apr_time_now());
+  char *cookie = apr_psprintf(r->pool, "%s=%s^%lld^%s;path=%s", conf->cookie_name, user, now, 
+                              auth_remote_signature(r->pool, user, now, auth_remote_salt), 
+                              conf->cookie_path);
+  apr_table_addn(r->err_headers_out, "Set-Cookie", cookie);
+}
+
+static authn_status do_remote_auth(request_rec *r, const char *user, const char *passwd, 
+                                   auth_remote_config_rec *conf)
+{
+  int rz;
+  char *user_pass, *b64_user_pass, *req;
+  unsigned char *rbuf;
+  apr_socket_t *rsock;
+  apr_sockaddr_t *addr;
+  apr_status_t rv;
+  
+  /* we were not configured */
+  if (conf->remote_port == NOT_CONFIGURED) {
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, APR_SUCCESS, r, "remote_auth was not configured for uri %s", 
+                  r->unparsed_uri);
+    return AUTH_USER_NOT_FOUND;
+  }
+  
+  rv = apr_socket_create(&rsock, APR_INET, SOCK_STREAM, APR_PROTO_TCP, r->pool);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "failed to create socket");
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  rv = apr_socket_timeout_set(rsock, DEFAULT_TIMEOUT);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "failed to set timeout on socket");
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  rv = apr_sockaddr_info_get(&addr, conf->remote_server, APR_INET, (apr_port_t)conf->remote_port, 
+                             0, r->pool);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "failed to setup sockaddr for %s:%d", 
+                  conf->remote_server, conf->remote_port);
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  
+  /* the base64 encoded Authorization header */
+  user_pass = apr_pstrcat(r->pool, user, ":", passwd, NULL);
+  b64_user_pass = apr_palloc(r->pool, apr_base64_encode_len(strlen(user_pass)) + 1);
+  apr_base64_encode(b64_user_pass, user_pass, strlen(user_pass));
+
+  /* the http request for the remote end */
+  req = apr_psprintf(r->pool, "HEAD %s HTTP/1.0%sAuthorization: Basic %s%s%s", conf->remote_path, 
+                     CRLF, b64_user_pass, CRLF, CRLF);
+
+  /* send the request to the remote server */
+  rv = apr_socket_connect(rsock, addr);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "failed to connect to remote server %s:%d", 
+                  conf->remote_server, conf->remote_port);
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  rz = strlen(req);
+  rv = apr_socket_send(rsock, (const char *)req, (apr_size_t *)&rz);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "write() to remote server failed");
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+
+  /* read the response from the remote end, 20 bytes should be enough parse the remote server's intent */
+  rbuf = apr_palloc(r->pool, rz);
+  rz = 20;
+  rv = apr_socket_recv(rsock, (char *)rbuf, (apr_size_t *)&rz);
+  apr_socket_close(rsock);
+  if (rv != APR_SUCCESS) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, "recv() from remote server failed");
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  if (rz < 13) {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "non HTTP reply from remote server");
+    return HTTP_INTERNAL_SERVER_ERROR;
+  }
+  if (apr_toupper(rbuf[0]) == 'H' && apr_toupper(rbuf[1]) == 'T' && apr_toupper(rbuf[2]) == 'T' 
+      && apr_toupper(rbuf[3]) == 'P' && rbuf[8] == ' ' && rbuf[9] == '2') {
+    return AUTH_GRANTED;
+  }
+  return AUTH_DENIED;
+}
+
+static authn_status check_authn(request_rec *r, const char *user, const char *passwd)
+{
+  const char *cookies;
+  authn_status remote_status = AUTH_DENIED;
+  auth_remote_config_rec *conf = ap_get_module_config(r->per_dir_config, &auth_remote_module);
+
+  /* no auth cookie was configured, authn against remote server */
+  if (conf->cookie_life == NOT_CONFIGURED) {
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, "cookie is not configured");
+    return do_remote_auth(r, user, passwd, conf);
+  }
+
+  /* cookie is configured, check if a cookie came in */
+  cookies = apr_table_get(r->headers_in, "Cookie");
+  if (cookies) {
+    char *our_cookie = ap_strstr((char *)cookies, conf->cookie_name);
+    if (our_cookie && auth_remote_validate_cookie(r, user, our_cookie, conf)) {
+      return AUTH_GRANTED;
+    }
+  }
+  
+  /** If our cookie 
+   *    -> didn't come in -or-
+   *    -> it came in but has expired -or-
+   *    -> we detect user tampering;
+   *  in all these cases always force a remote authentication and reset the cookie if needed 
+   **/
+  remote_status = do_remote_auth(r, user, passwd, conf);
+  if (remote_status == AUTH_GRANTED) {
+    auth_remote_set_cookie(r, user, conf);
+  }
+  return remote_status;
+}
+
+static int auth_remote_init_module(apr_pool_t *p, apr_pool_t *plog, apr_pool_t *ptem, server_rec *s)
+{
+  void *was_here;
+  apr_status_t rv;
+
+  if (!want_salt)
+    return OK;
+
+  apr_pool_userdata_get(&was_here, "auth_remote_key", s->process->pool);
+  if (!was_here) {
+    apr_pool_userdata_set((void *)1, "auth_remote_key", apr_pool_cleanup_null, s->process->pool);
+    return OK;
+  }
+
+  rv = apr_generate_random_bytes(auth_remote_salt, sizeof(auth_remote_salt));
+  if (rv != APR_SUCCESS) {
+    ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, "auth_remote: could not generate random salt");
+    return !OK;
+  }
+  ap_log_error(APLOG_MARK, APLOG_DEBUG, 0, s, "secret salt generated");
+  return OK;
+}
+
+static const authn_provider auth_remote_provider =
+  {
+    &check_authn,
+    NULL
+  };
+
+static void register_hooks(apr_pool_t *p)
+{
+#ifndef AUTH_REMOTE_NO_SALT
+#ifndef APR_HAS_RANDOM
+#error APR random number support is needed to generate secret salt
+#endif
+  ap_hook_post_config(auth_remote_init_module, NULL, NULL, APR_HOOK_MIDDLE);
+#endif
+  ap_register_provider(p, AUTHN_PROVIDER_GROUP, "remote", "0", &auth_remote_provider);
+}
+
+
+module AP_MODULE_DECLARE_DATA auth_remote_module = 
+  {
+    STANDARD20_MODULE_STUFF,
+    create_auth_remote_dir_config,
+    NULL,
+    NULL,
+    NULL,
+    auth_remote_cmds,
+    register_hooks
+  };
+
